e261bb338f28f3d21905b7de763abcdfcb51312e
git-zahidul-islam <freelancerzahidf101@gmail.com>
2025-01-22 08:05:59 +0600

    navbar scroll smooth

--- src/components/ui/manu/Manu.jsx
+++ src/components/ui/manu/Manu.jsx
@@ -1,15 +1,25 @@
-const Manu = ({manuName,address}) => {
+const Manu = ({ manuName, address }) => {
+    const handleScroll = (e) => {
+      e.preventDefault(); // Prevent the default anchor behavior
+      const targetElement = document.querySelector(address);
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: "smooth", block: "start" });
+      }
+    };
+  
     return (
-        <>
-            <a
-                className="text-xl font-normal relative group hover:text-[#FD6F00] transition-all duration-300 ease-in-out"
-                href={address}
-            >
-                {manuName}
-                <span className="absolute bottom-0 left-0 w-0 h-[2px] bg-black transition-all duration-300 ease-in-out group-hover:w-full group-hover:right-0"></span>
-            </a>
-        </>
+      <>
+        <a
+          className="text-xl font-normal relative group hover:text-[#FD6F00] transition-all duration-300 ease-in-out"
+          href={address}
+          onClick={handleScroll} // Attach the handler here
+        >
+          {manuName}
+          <span className="absolute bottom-0 left-0 w-0 h-[2px] bg-black transition-all duration-300 ease-in-out group-hover:w-full group-hover:right-0"></span>
+        </a>
+      </>
     );
-};
-
-export default Manu;
+  };
+  
+  export default Manu;
+  
